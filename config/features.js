610dcf50099c1f30108fed70a37fa2e640ddb0c5
Vesa Meskanen <vesa.meskanen@cgi.com>
2022-03-10 17:59:06 +0200

    add debug logging

--- config/features.js
+++ config/features.js
@@ -46,6 +46,8 @@ if(process.env.OSM_VENUE_FILTERS) {
   venue_filters = venue_filters.concat(JSON.parse(process.env.OSM_VENUE_FILTERS));
 }
 
+console.log(JSON.stringify(venue_filters));
+
 var address_filters = [
   { access: 'no' }
 ];
